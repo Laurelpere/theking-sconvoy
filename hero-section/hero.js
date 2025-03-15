8451e0ff1aaa08050c06c3dd44ec7c393d69e808
Laurelpere <pelau360@gmail.com>
2025-03-15 02:37:17 +0100

    update

--- hero-section/hero.js
+++ hero-section/hero.js
@@ -3,18 +3,24 @@ const open = document.querySelector(".open");
 const close = document.querySelector(".close");
 const hidden = document.querySelector(".hidden")
 const nav = document.querySelector("ul")
+const header = document.querySelector("header")
 
 
 const openFunc = function () {
-    open.classList.add("hidden");
-    close.classList.remove("hidden");
+    // open.classList.add("hidden");
+    // close.classList.remove("hidden");
+    open.style.display = "none";
+    close.style.display = "block";
     overlay.classList.remove("hidden");
     nav.style.visibility = "visible";
 }
 
 const closeFunc = function () {
-    open.classList.remove("hidden");
-    close.classList.add("hidden");
+
+    // open.classList.remove("hidden");
+    // close.classList.add("hidden");
+    open.style.display = "block";
+    close.style.display = "none";
     overlay.classList.add("hidden");
     nav.style.visibility = "hidden";
 }
